90cf846b5989097bd5fbf4241cc2b8d7cbd9fba4
Suraj <info.blogtheorem@gmail.com>
2021-08-17 20:32:17 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,55 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Moment from "react-moment";
 
 export default function Home() {
+  const [startDate, setStartDate] = useState();
+  const date = new Date();
+
+  const handleClick = () => {
+    console.log("Click Button");
+  };
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Ticket 120</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="m-4">
+        <p>Ticket 120 Date Calculator</p>
+        <div>Enter Journey Date</div>
+        <div>
+          <DatePicker
+            selected={startDate}
+            dateFormat="dd/MM/yyyy"
+            onChange={(date) => setStartDate(date)}
+          />
         </div>
-      </main>
+        <br />
+        {/* <div>
+          <button className="bg-green-500 p-2 mt-2" onClick={handleClick}>
+            Check Date
+          </button>
+        </div> */}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div>Book ticket on this date</div>
+
+        {startDate !== undefined ? (
+          <div>
+            <Moment format="D MMM YYYY" withTitle subtract={{ days: 120 }}>
+              {startDate}
+            </Moment>
+            - 8:00AM IST
+          </div>
+        ) : (
+          <p></p>
+        )}
+      </main>
+      <footer></footer>
     </div>
-  )
+  );
 }
