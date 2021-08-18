f2d67b26d7fc5c49868206cfcc0a2e2fe5bab325
Suraj <info.blogtheorem@gmail.com>
2021-08-18 20:30:16 +0530

    seo

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,124 @@
-import Head from "next/head";
 import React, { useState } from "react";
+import Image from "next/image";
+
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
 import Moment from "react-moment";
+import logo from "../public/logo.png";
 
-export default function Home() {
+import Head from "next/head";
+
+function ticket() {
+  // DATE HOOK
   const [startDate, setStartDate] = useState();
-  const date = new Date();
-
-  const handleClick = () => {
-    console.log("Click Button");
-  };
 
   return (
-    <div>
-      <Head>
-        <title>Ticket 120</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="m-4">
-        <p>Ticket 120 Date Calculator</p>
-        <div>Enter Journey Date</div>
-        <div>
-          <DatePicker
-            selected={startDate}
-            dateFormat="dd/MM/yyyy"
-            onChange={(date) => setStartDate(date)}
+    <>
+      <div className="container">
+        <Head>
+          <link rel="icon" href="/logo.png" />
+          <title>
+            ticket120 - IRCTC TRAIN TICKET ADVANCED BOOKING DATE CALCULATOR
+          </title>
+          <meta
+            name="description"
+            content="अपनी बुकिंग से 120 दिन पहले की उन्नत तारीख का तुरंत पता लगाएं टिकट120 के माध्यम से रेल यात्रा।"
           />
-        </div>
-        <br />
-        {/* <div>
-          <button className="bg-green-500 p-2 mt-2" onClick={handleClick}>
-            Check Date
-          </button>
-        </div> */}
+          <meta name="keywords" content="train advanced booking date" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://ticket120.toolcarton.com" />
+          <meta
+            property="og:title"
+            content="ticket120 - IRCTC TRAIN TICKET ADVANCED BOOKING DATE CALCULATOR"
+          />
+          <meta
+            property="og:description"
+            content="अपनी बुकिंग से 120 दिन पहले की उन्नत तारीख का तुरंत पता लगाएं टिकट120 के माध्यम से रेल यात्रा।"
+          />
+          <meta
+            property="og:image"
+            content="https://ticket120.toolcarton.com/banner.png"
+          />
 
-        <div>Book ticket on this date</div>
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://ticket120.toolcarton.com/"
+          />
+          <meta
+            property="twitter:title"
+            content="ticket120 - IRCTC TRAIN TICKET ADVANCED BOOKING DATE CALCULATOR"
+          />
+          <meta
+            property="twitter:description"
+            content="अपनी बुकिंग से 120 दिन पहले की उन्नत तारीख का तुरंत पता लगाएं टिकट120 के माध्यम से रेल यात्रा।"
+          />
+          <meta
+            property="twitter:image"
+            content="https://ticket120.toolcarton.com/banner.png"
+          />
 
-        {startDate !== undefined ? (
-          <div>
-            <Moment format="D MMM YYYY" withTitle subtract={{ days: 120 }}>
-              {startDate}
-            </Moment>
-            - 8:00AM IST
+          <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo.png" />
+          <link rel="canonical" href="https://ticket120.toolcarton.com/" />
+          <meta name="google-site-verification" content="" />
+        </Head>
+        <nav>
+          <Image src={logo} alt="ticket120 logo" width={50} height={10} />
+          <h3>ticket120</h3>
+        </nav>
+        <main>
+          <h1>🚆 IRCTC TRAIN TICKET ADVANCED BOOKING DATE CALCULATOR</h1>
+          <p className="para2">
+            अपनी बुकिंग से 120 दिन पहले की उन्नत तारीख का तुरंत पता लगाएं
+            टिकट120 के माध्यम से रेल यात्रा। वेबसाइट का उपयोग करना बहुत आसान है,
+            बस इनपुट सेक्शन से नीचे अपनी यात्रा की तारीख चुनें।
+          </p>
+          <h3>SELECT JOURNEY DATE </h3>
+          <div className="dateContainer">
+            <DatePicker
+              selected={startDate}
+              dateFormat="dd/MM/yyyy"
+              onChange={(date) => setStartDate(date)}
+            />
           </div>
-        ) : (
-          <p></p>
-        )}
-      </main>
-      <footer></footer>
-    </div>
+          {startDate !== undefined ? (
+            <div>
+              <p>👇 इस तारीख को टिकट बुक करें 👇</p>
+              <h2>
+                <Moment format="D MMM YYYY" withTitle subtract={{ days: 120 }}>
+                  {startDate}
+                </Moment>
+                - 8:00AM to 10:00AM IST
+              </h2>
+              <button>
+                <a
+                  href="https://www.irctc.co.in/nget/train-search"
+                  target="_blank"
+                >
+                  {" "}
+                  Book through IRCTC
+                </a>
+              </button>
+              <p className="para1">
+                इस तरह ऊपर बताई गई तारीख से आप अपनी बुकिंग शुरू कर सकते हैं
+                वास्तविक यात्रा तिथि से 120 दिन पहले रेल यात्रा। कृपया जांचें एक
+                बार आधिकारिक आईआरसीटीसी साइट से।
+              </p>
+            </div>
+          ) : (
+            <p></p>
+          )}{" "}
+        </main>
+      </div>
+      <footer>
+        <p>
+          <a href="https://blogtheorem.com">&copy; blogtheorem</a> द्वारा
+          निर्मित
+        </p>
+      </footer>
+    </>
   );
 }
+
+export default ticket;
